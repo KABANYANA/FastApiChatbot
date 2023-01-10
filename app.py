5ef0113c5238b292ab9876e30a590a37e419279a
KABANYANA <joyeusekabanyana@gmail.com>
2023-01-10 09:26:05 +0200

    updated backend

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ main  = FastAPI()
 @main.get('/') #this the path/route
 def index():
     return "This is the front page"
+# this is a page
+@main.get('about') #this the path/route
+def index():
+    return "This is the about page"
+# this is backend
